73471a8e18c8a8c2a019740c627b35e80818d2de
Ragil14 <rahil.gilani@gmail.com>
2022-03-23 14:59:17 -0500

    add plugins for jQuery npm package & require webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 
 // main configuration object
 module.exports = {
@@ -7,5 +8,11 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js'
     },
+    plugins:[
+        new webpack.ProvidePlugin({
+          $: "jquery",
+          jQuery: "jquery"
+        }),
+    ],
     mode: 'development'
 };
