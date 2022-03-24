3908362be1672b71d3caf17975b65ed6b7ae1b5f
Ragil14 <rahil.gilani@gmail.com>
2022-03-24 18:51:46 -0500

    moved dateConverter() function from script.js to helpers.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,6 @@ require("bootstrap");
 
 
 
-  dateConverter = function(UNIX_timestamp) {
-    const a = new Date(UNIX_timestamp);
-    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    const year = a.getFullYear();
-    const month = months[a.getMonth()];
-    const date = a.getDate();
-    const result =  month + ' ' + date + ', ' + year 
-    return result;
-  };
-
 
   // First image is hard coded in index.html
   const carouselSlides = [
