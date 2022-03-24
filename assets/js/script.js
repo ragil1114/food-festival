51cd19196dea544464111b8970824b19cb7b465a
Ragil14 <rahil.gilani@gmail.com>
2022-03-24 18:36:46 -0500

    move conditional statement from script.js to tickets.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -159,7 +159,7 @@ $(document).ready(function() {
     pageEl.appendChild(containerEl2);
     pageEl.appendChild(containerEl3);
   }
-  
+
 
   dateConverter = function(UNIX_timestamp) {
     const a = new Date(UNIX_timestamp);
@@ -171,29 +171,7 @@ $(document).ready(function() {
     return result;
   };
 
-  if (window.location.href.indexOf("tickets") > -1) {
 
-    const purchaseBtn = document.getElementById("purchaseBtn");
-    const purchaseEmail = document.getElementById("purchaseEmail");
-    const modalEl = document.querySelector(".modal-content");
-    const modalBodyEl = document.querySelector(".modal-body");
-    const modalFooterEl = document.querySelector(".modal-footer");
-
-
-    function purchaseTicket () {
-
-      modalEl.removeChild(modalBodyEl)
-      modalEl.removeChild(modalFooterEl)
-
-      modalEl.append(createEl("div", {class: "modal-body"},
-        createEl("h5", {class: "modal-title"}, 
-        `Thanks for requesting a ticket purchase! We will send an email to ${purchaseEmail.value} to complete the order form!`
-        ),
-      ))
-      
-    }
-    purchaseBtn.addEventListener("click", purchaseTicket);
-  }
   // First image is hard coded in index.html
   const carouselSlides = [
     {
