147f08097765708718bbbff697a91c1adbdf5081
Ragil14 <rahil.gilani@gmail.com>
2022-03-24 16:48:38 -0500

    import webpack-bundle-analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const webpack = require("webpack");
 
 // main configuration object
