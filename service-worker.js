9082726e5b37e79ddc8f9c2f474ff6f726d1c9ff
Ragil14 <rahil.gilani@gmail.com>
2022-03-25 15:47:46 -0500

    add files to precache & Event Listener to install SW

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./events.html",
+    "./tickets.html",
+    "./schedule.html",
+    "./assets/css/style.css",
+    "./assets/css/bootstrap.css",
+    "./assets/css/tickets.css",
+    "./dist/app.bundle.js",
+    "./dist/events.bundle.js",
+    "./dist/tickets.bundle.js",
+    "./dist/schedule.bundle.js"
+];
+
+self.addEventListener('install', function (e) {
+
+})
